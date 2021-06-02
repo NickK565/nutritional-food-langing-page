57440e2b6eaee1c19e0ddd3f4563a86aa478dd8a
NickK565 <recamy10@gmail.com>
2021-06-02 11:54:22 +0600

    Added axios library and used instead of get resourse function

--- js/script.js
+++ js/script.js
@@ -193,9 +193,16 @@ window.addEventListener('DOMContentLoaded', function() {
     };
 
 
-    getResourse('http://localhost:3000/menu')
+    /* getResourse('http://localhost:3000/menu')
         .then(data => {
             data.forEach(({img, altimg, title, descr, price}) => {
+                new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
+            });
+        }); */
+
+    axios.get('http://localhost:3000/menu')
+        .then(data =>{
+            data.data.forEach(({img, altimg, title, descr, price}) => {
                 new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
             });
         });
