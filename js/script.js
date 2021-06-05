cdd1b7252b7852eb894fe418bb3c003c506d193e
oldman <recamy10@gmail.com>
2021-06-05 13:48:59 +0600

    simplified some functions and added regular expressions

--- js/script.js
+++ js/script.js
@@ -290,13 +290,7 @@ window.addEventListener('DOMContentLoaded', function() {
         width = window.getComputedStyle(slidesWrapper).width,
         slidesField = document.querySelector('.offer__slider-inner');
 
-    if (slides.length < 10) {
-        total.textContent = `0${slides.length}`;
-        current.textContent =  `0${slideIndex}`;
-    } else {
-        total.textContent = slides.length;
-        current.textContent =  slideIndex;
-    }
+    slidesLength();
     
     slidesField.style.width = 100 * slides.length + '%';
     slidesField.style.display = 'flex';
@@ -367,14 +361,9 @@ window.addEventListener('DOMContentLoaded', function() {
             slideIndex++;
         }
 
-        if (slides.length < 10) {
-            current.textContent =  `0${slideIndex}`;
-        } else {
-            current.textContent =  slideIndex;
-        }
+        slidesLength();
 
-        dots.forEach(dot => dot.style.opacity = ".5");
-        dots[slideIndex-1].style.opacity = 1;
+        dotsOpacity();
     });
 
     prev.addEventListener('click', () => {
@@ -392,14 +381,9 @@ window.addEventListener('DOMContentLoaded', function() {
             slideIndex--;
         }
 
-        if (slides.length < 10) {
-            current.textContent =  `0${slideIndex}`;
-        } else {
-            current.textContent =  slideIndex;
-        }
+        slidesLength();
 
-        dots.forEach(dot => dot.style.opacity = ".5");
-        dots[slideIndex-1].style.opacity = 1;
+        dotsOpacity();
     });
 
     dots.forEach(dot => {
@@ -411,16 +395,24 @@ window.addEventListener('DOMContentLoaded', function() {
 
             slidesField.style.transform = `translateX(-${offset}px)`;
 
-            if (slides.length < 10) {
-                current.textContent =  `0${slideIndex}`;
-            } else {
-                current.textContent =  slideIndex;
-            }
+            slidesLength();
 
-            dots.forEach(dot => dot.style.opacity = ".5");
-            dots[slideIndex-1].style.opacity = 1;
+            dotsOpacity();
         });
     });
+
+    function slidesLength() {
+        if (slides.length < 10) {
+            current.textContent =  `0${slideIndex}`;
+        } else {
+            current.textContent =  slideIndex;
+        }
+    }
+
+    function dotsOpacity() {
+        dots.forEach(dot => dot.style.opacity = ".5");
+        dots[slideIndex-1].style.opacity = 1;
+    }
 
     function deleteNotDigits(str) {
         return +str.replace(/\D/g, '');
